1c13712387d3ce4a62b55ed8b3bae4687dbd63c0
Volzyfp <52263392+Volzyfp@users.noreply.github.com>
2019-06-27 15:38:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
    console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
+      console.log(`Cyhper Bot Script By : Lumiz`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Lumiz ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
-client.user.setStatus("dnd")
+client.user.setStatus("Done")
 });
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
